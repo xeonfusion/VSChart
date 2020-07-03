22f2ef754eeeea9c118bbee3841273aa76f3a827
xeonfusion <xeonfusion@yahoo.com>
2020-07-03 20:52:18 +0400

    trial grid data

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React, { Component } from "react";
 //import logo from "./logo.svg";
 import "./App.css";
 import AnaesthesiaChart from "./anaesthchart.jsx";
-import MedicationGrid from "./medicationgrid.jsx";
+import MedicationGrid2 from "./medicationgrid.jsx";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <MedicationGrid />
+        <MedicationGrid2 />
         <AnaesthesiaChart />
       </div>
     );
--- src/medicationgrid1.jsx
+++ src/medicationgrid1.jsx
@@ -30,6 +30,18 @@ class MedicationGrid extends React.Component {
       rowsdata: [],
     };
   }
+
+  componentDidMount() {
+    var data = [
+      { drug: "Propofol", dose: 150, unit: "mg", time: 1500 },
+      { drug: "Fentanyl", dose: 75, unit: "mcg", time: 1500 },
+      { drug: "Rocuronium", dose: 50, unit: "mg", time: 1505 },
+      { drug: "Lidocaine", dose: 100, unit: "mg", time: 1500 },
+      { drug: "Ringers", dose: 60, unit: "ml/hr", time: 1500, timespan: 60 },
+    ];
+    this.setState({ rowsdata: data });
+  }
+
   render() {
     return (
       <div style={divStyle}>
@@ -37,7 +49,6 @@ class MedicationGrid extends React.Component {
           columns={columns}
           rows={rows}
           rowsCount={10}
-          rowGetter={(i) => rows[i]}
           enableCellSelect={true}
         />
       </div>
