3f50c9e40e0efdc0eb2430070242e7fb7ededffb
xeonfusion <xeonfusion@yahoo.com>
2020-11-13 17:44:09 +0400

    appmenu update
    appmenu update

--- src/appmenu.jsx
+++ src/appmenu.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
 import Drawer from "@material-ui/core/Drawer";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import AppBar from "@material-ui/core/AppBar";
@@ -8,14 +8,25 @@ import List from "@material-ui/core/List";
 import Typography from "@material-ui/core/Typography";
 //import Divider from "@material-ui/core/Divider";
 import Button from "@material-ui/core/Button";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import clsx from "clsx";
+
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+import ChevronRightIcon from "@material-ui/icons/ChevronRight";
 
 import AnaesthesiaChart from "./anaesthchart.jsx";
 import MedicationGrid2 from "./medicationgrid2.jsx";
-import { ListItem } from "@material-ui/core";
+import { ListItem, ListItemText } from "@material-ui/core";
+import AddBoxIcon from "@material-ui/icons/AddBox";
+import ZoomInIcon from "@material-ui/icons/ZoomIn";
+import AutorenewIcon from "@material-ui/icons/Autorenew";
 
-const drawerWidth = 150;
+const drawerWidth = 240;
+const minidrawerWidth = 70;
 
-const useStyles = makeStyles((theme) => ({
+/*const useStyles = makeStyles((theme) => ({
   root: {
     //display: "flex",
     display: "block",
@@ -39,6 +50,71 @@ const useStyles = makeStyles((theme) => ({
     width: `calc(100% - ${drawerWidth}px)`,
     padding: theme.spacing(0),
   },
+}));*/
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    //display: "flex",
+    display: "block",
+  },
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
+    transition: theme.transitions.create(["width", "margin"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  appBarShift: {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(["width", "margin"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  menuButton: {
+    marginRight: 36,
+  },
+  hide: {
+    display: "none",
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+    whiteSpace: "nowrap",
+  },
+  drawerOpen: {
+    width: drawerWidth,
+    transition: theme.transitions.create("width", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  drawerClose: {
+    transition: theme.transitions.create("width", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    overflowX: "hidden",
+    width: theme.spacing(7) + 1,
+    [theme.breakpoints.up("sm")]: {
+      width: theme.spacing(9) + 1,
+    },
+  },
+  toolbar: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "flex-end",
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+  },
+  content: {
+    //flexGrow: 1,
+    marginLeft: minidrawerWidth,
+    width: `calc(100% - ${minidrawerWidth}px)`,
+    padding: theme.spacing(0),
+  },
 }));
 
 export default function AppMenu() {
@@ -46,54 +122,93 @@ export default function AppMenu() {
   const chartRef = React.useRef();
 
   const classes = useStyles();
+  const theme = useTheme();
+
+  const [open, setOpen] = React.useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
 
   return (
     <div className={classes.root}>
       <CssBaseline />
-      <AppBar position="fixed" className={classes.appBar}>
+      <AppBar
+        position="fixed"
+        className={clsx(classes.appBar, {
+          [classes.appBarShift]: open,
+        })}
+      >
         <Toolbar>
-          <Typography variant="h6" noWrap>
-            VSChart
-          </Typography>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            className={clsx(classes.menuButton, {
+              [classes.hide]: open,
+            })}
+          >
+            <MenuIcon />
+          </IconButton>
         </Toolbar>
       </AppBar>
       <Drawer
-        className={classes.drawer}
         variant="permanent"
-        anchor="left"
+        className={clsx(classes.drawer, {
+          [classes.drawerOpen]: open,
+          [classes.drawerClose]: !open,
+        })}
         classes={{
-          paper: classes.drawerPaper,
+          paper: clsx({
+            [classes.drawerOpen]: open,
+            [classes.drawerClose]: !open,
+          }),
         }}
       >
+        <div className={classes.toolbar}>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === "rtl" ? (
+              <ChevronRightIcon />
+            ) : (
+              <ChevronLeftIcon />
+            )}
+          </IconButton>
+        </div>
+
         <Toolbar />
         <div className={classes.toolbar} />
         <List>
-          <ListItem>
-            <Button
-              variant="contained"
-              color="primary"
-              onClick={() => medgridRef.current.handleShowMedCall()}
-            >
-              Add Medication
-            </Button>
+          <ListItem
+            button
+            onClick={() => medgridRef.current.handleShowMedCall()}
+          >
+            <ListItemIcon>
+              <AddBoxIcon />
+            </ListItemIcon>
+            <ListItemText>Add Medication</ListItemText>
           </ListItem>
-          <ListItem>
-            <Button
-              variant="contained"
-              color="primary"
-              onClick={() => medgridRef.current.handleTimeStepsCall()}
-            >
-              Change Timesteps
-            </Button>
+          <ListItem
+            button
+            onClick={() => medgridRef.current.handleTimeStepsCall()}
+          >
+            <ListItemIcon>
+              <ZoomInIcon />
+            </ListItemIcon>
+            <ListItemText>Change Timesteps</ListItemText>
           </ListItem>
-          <ListItem>
-            <Button
-              variant="contained"
-              color="primary"
-              onClick={() => chartRef.current.handleLoadChartCall()}
-            >
-              Load Chart
-            </Button>
+          <ListItem
+            button
+            onClick={() => chartRef.current.handleLoadChartCall()}
+          >
+            <ListItemIcon>
+              <AutorenewIcon />
+            </ListItemIcon>
+            <ListItemText>Load Chart</ListItemText>
           </ListItem>
         </List>
       </Drawer>
